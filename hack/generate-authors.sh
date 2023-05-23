1f2fea3d9ad630087fcdc2f0a5eb8fdda6992d3c
Andrew Kibet <tanuilelan@gmail.com>
2023-05-23 02:35:22 -0700

    Authors File

--- hack/generate-authors.sh
+++ hack/generate-authors.sh
@@ -2,8 +2,6 @@
 
 contributors=$(git log --format='%aN <%aE>' | sort -fu)
 authors_file="AUTHORS"
-header="# CONTRIBUTORS\n\n"
-content="${header}${contributors}"
-echo -e "$content" > "$authors_file"
+echo "$contributors" > "$authors_file"
 echo "AUTHORS file generated successfully."
 
