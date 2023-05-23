73e40be41fd7a1846c602920091ba91ede21ef01
Andrewtanui <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@gmail.com>
2023-05-23 01:16:55 -0700

    Task updated

--- shell.c
+++ shell.c
@@ -1,35 +1,34 @@
 #include "shell.h"
+
 /**
  * main - Entry point of the shell
  *
  * Return: Always 0
  */
-
 int main(void)
 {
-    char *line;
-    char **args;
-    int status;
+	char *line;
+	char **args;
+	int status;
 
-    do {
-        printf("#cisfun$ ");
-        line = read_line();
-        args = split_line(line);
+	do {
+		printf("#cisfun$ ");
+		line = read_line();
+		args = split_line(line);
 
-        if (args[0] != NULL && strcmp(args[0], "cd") == 0)
-        {
-            status = ex_cd(args);
-        }
-        else
-        {
-            status = ex_command(args);
-        }
+		if (args[0] != NULL && strcmp(args[0], "cd") == 0)
+		{
+			status = ex_cd(args);
+		}
+		else
+		{
+			status = ex_command(args);
+		}
 
-        free(line);
-        free(args);
-    } while (status);
+		free(line);
+		free(args);
+	} while (status);
 
-    return 0;
+	return (0);
 }
-
 
