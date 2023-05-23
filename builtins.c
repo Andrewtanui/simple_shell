455b04ea2bb7c4d8afdb11249ef014b9f6a81069
Andrewtanui <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@gmail.com>
2023-05-23 01:37:03 -0700

    Task updates

--- builtins.c
+++ builtins.c
@@ -1,54 +1,53 @@
 #include "shell.h"
-/**
- * ex_exit - exit built-in command
- *
- * Return: Returns 0 to exit shell
- */
 
+/**
+ * ex_exit - Exit built-in command
+ *
+ * Return: Always returns 0 to exit shell
+ */
 int ex_exit(void)
 {
-    return 0;
+	return (0);
 }
-/**
- * ex_env - env built-in command
- *
- * Return: Returns 1 to continue the shell
- */
 
+/**
+ * ex_env - Env built-in command
+ *
+ * Return: Always returns 1 to continue the shell
+ */
 int ex_env(void)
 {
-    extern char **environ;
-    int i = 0;
+	int i = 0;
 
-    while (environ[i])
-    {
-        printf("%s\n", environ[i]);
-        i++;
-    }
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
 
-    return 1;
+	return (1);
 }
+
 /**
- * ex_cd - cd built-in command
+ * ex_cd - Cd built-in command
  * @args: Args passed as parameters
  *
  * Return: 1 to continue with the shell
  */
-
 int ex_cd(char **args)
 {
-    if (args[1] == NULL)
-    {
-        fprintf(stderr, "cd: expected argument to \"cd\"\n");
-    }
-    else
-    {
-        if (chdir(args[1]) != 0)
-        {
-            perror("cd");
-        }
-    }
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "cd: expected argument to \"cd\"\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("cd");
+		}
+	}
 
-    return 1;
+	return (1);
 }
 
